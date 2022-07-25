12c7f6464798731bb31b1854fd5b10242e3bcbde
Benjamin Engen <benjamin.engen@gmail.com>
2022-07-25 09:37:28 +0200

    fix: fixed voter file load error that was also grabbing requirements.txt

--- load_co_voter_file.py
+++ load_co_voter_file.py
@@ -18,17 +18,17 @@ def extract_co_voter_file():
 
 def load_co_voter_file():
     files_list = os.listdir()
-    files_list = [file for file in files_list if '.txt' in file]
+    files_list = [file for file in files_list if 'voters' in file.lower()]
     _df = pd.DataFrame()
     _list = []
     
     for file in files_list:
+        print(f"Loading file: {file}")
         _df = pd.read_csv(file, sep=",", encoding='cp437', index_col=None, header=0, low_memory=False, error_bad_lines=False)
         _list.append(_df)
     
     voter_file_df = pd.concat(_list)
     voter_file_df.reset_index(drop=True, inplace=True)
-    voter_file_df['VOTER_ID'] =  _df['VOTER_ID'].astype('float').astype('int64').astype('str')
-    del voter_file_df['backcall==0.2.0']
+    voter_file_df['VOTER_ID'] =  voter_file_df['VOTER_ID'].astype('float').astype('int64').astype('str')
 
     return voter_file_df
