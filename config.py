91561d7868c5b858c0d5bb40c2bb3c740d78d764
Benjamin Engen <benjamin.engen@gmail.com>
2022-10-27 15:44:54 +0100

    feature: switched to sharded table structure

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ ftp_directory = r'/EX-003 Master Voter List'
 bq_project_id = os.environ.get('BQ_PROJECT_ID')
 
 bq_table_stem = bq_project_id + '.co_voterfile.'
-bq_table_id = bq_table_stem + str(date.today().year) + '-' f"{date.today().month:02d}"
+bq_table_id = bq_table_stem + 'voters_' +str(date.today().year) + f"{(date.today().month - 1):02d}" + '01'
 
 # Establish BigQuery credentials
 bq_account_creds = json.loads(os.environ.get('BQ_ACCOUNT_CREDS'))
